42a161c6772374cf75deebc955f464c2bb0ff6c9
comye1 <comye1@seoultech.ac.kr>
2022-03-16 21:53:53 +0900

    #1 MainActivity (AndroidEntryPoint 어노테이션)

--- app/src/main/java/com/comye1/capstone/MainActivity.kt
+++ app/src/main/java/com/comye1/capstone/MainActivity.kt
@@ -1,35 +1,18 @@
 package com.comye1.capstone
 
 import android.os.Bundle
-import android.provider.ContactsContract
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.rememberNavController
 import com.comye1.capstone.navigation.BottomNavigationBar
 import com.comye1.capstone.navigation.Navigator
-import com.comye1.capstone.screens.FeedScreen
-import com.comye1.capstone.screens.NewPlayLog
 import com.comye1.capstone.ui.theme.CapstoneTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
