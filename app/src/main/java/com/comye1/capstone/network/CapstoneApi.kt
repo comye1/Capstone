16dbbf8837ea31619945b6021faa338e8a33a18e
comye1 <comye1@seoultech.ac.kr>
2022-05-22 21:42:08 +0900

    api 추가

--- app/src/main/java/com/comye1/capstone/network/CapstoneApi.kt
+++ app/src/main/java/com/comye1/capstone/network/CapstoneApi.kt
@@ -1,20 +1,58 @@
 package com.comye1.capstone.network
 
-import com.comye1.capstone.network.models.Response
-import com.comye1.capstone.network.models.SignUpBody
-import com.comye1.capstone.network.models.SignUpData
-import retrofit2.http.GET
-import retrofit2.http.HeaderMap
-import retrofit2.http.POST
+import com.comye1.capstone.network.models.*
+import retrofit2.http.*
 
 interface CapstoneApi {
-    @POST
-    suspend fun signUpUser(signUpBody: SignUpBody): Response<SignUpData>
 
-    @GET
+    @GET("api/user/idcheck")
+    suspend fun idCheck(@Body body: IdBody): Response<Boolean>
+
+    @POST("api/user")
+    suspend fun signUpUser(@Body signUpBody: SignUpBody): Response<SignUpData>
+
+    @GET("api/user")
     suspend fun validUser(
         @HeaderMap header: Map<String, String>,
     ): Response<String>
+
+    @PATCH("api/user")
+    suspend fun changeUserNickname(
+        @HeaderMap header: Map<String, String>,
+        nickname: String,
+    ): Response<String>
+
+//    @GET("api/user/profile")
+//    suspend fun
+
+    @POST("api/auth")
+    suspend fun signInUser(
+        @Body signInBody: SignInBody
+    ): Response<String>
+
+    @POST("api/follow/{id}")
+    suspend fun followUser(
+        @HeaderMap header: Map<String, String>,
+        @Path("id") id: Int,
+    ): Response<FollowData>
+
+    @DELETE("api/follow/{id}")
+    suspend fun unfollowUser(
+        @HeaderMap header: Map<String, String>,
+        @Path("id") id: Int,
+    ): Response<String>
+
+    @GET("api/follow/following/{id}")
+    suspend fun getFollowings(
+        @HeaderMap header: Map<String, String>,
+        @Path("id") id: Int,
+    ): Response<FollowData>
+
+    @GET("api/follow/follower/{id}")
+    suspend fun getFollowers(
+        @HeaderMap header: Map<String, String>,
+        @Path("id") id: Int,
+    ): Response<FollowData>
 
 
 }
--- app/src/main/java/com/comye1/capstone/repository/CapstoneRepository.kt
+++ app/src/main/java/com/comye1/capstone/repository/CapstoneRepository.kt
@@ -2,11 +2,11 @@ package com.comye1.capstone.repository
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.util.Log
 import androidx.core.content.edit
 import com.comye1.capstone.network.CapstoneApi
 import com.comye1.capstone.network.Resource
-import com.comye1.capstone.network.models.SignUpBody
-import com.comye1.capstone.network.models.SignUpData
+import com.comye1.capstone.network.models.*
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 
@@ -20,25 +20,53 @@ class CapstoneRepository @Inject constructor(
         Context.MODE_PRIVATE
     )
 
-    private fun saveToken(prefix: String, token: String) {
+    private fun saveToken(token: String) {
         tokenSharedPref.edit {
-            putString("TOKEN", "$prefix $token")
+            putString("TOKEN", token)
             commit()
         }
     }
 
     private fun getSavedToken(): String? = tokenSharedPref.getString("TOKEN", null)
 
-    private fun String.toTokenMap(): Map<String, String> = mapOf(Pair("Authorization", this))
+    private fun String.toTokenMap(): Map<String, String> = mapOf(Pair("Authorization", "Bearer $this"))
 
-    suspend fun signUpUser(signUpBody: SignUpBody): Resource<SignUpData> {
+    suspend fun idCheck(id: String): Resource<Boolean> {
+        Log.d("repo 1", "called")
         val result = try {
-            api.signUpUser(signUpBody)
+            Log.d("repo 1", "success")
+            api.idCheck(IdBody(user_id = id))
+        }catch (e:Exception) {
+            Log.d("repo 1", "exception ${e.message.toString()}")
+            return Resource.Failure(message = e.message.toString())
+        }
+        Log.d("repo 1", result.success.toString())
+        return if (result.success)
+            Resource.Success(true)
+        else Resource.Failure(message = result.message)
+    }
+
+    suspend fun signUpUser(id:String, password: String, nickname: String): Resource<SignUpData> {
+        val result = try {
+            api.signUpUser(SignUpBody(id, password, nickname))
         }catch (e: Exception) {
             return Resource.Failure(message = e.message.toString())
         }
         return if (result.success)
             Resource.Success(data = result.data)
+        else Resource.Failure(message = result.message)
+    }
+
+    suspend fun signInUser(id: String, password: String): Resource<String> {
+        val result = try {
+            api.signInUser(signInBody = SignInBody(id, password))
+        } catch (e: Exception) {
+            return Resource.Failure(message = e.message.toString())
+        }
+        return if (result.success){
+            saveToken(result.data)
+            Resource.Success(data = result.data)
+        }
         else Resource.Failure(message = result.message)
     }
 
@@ -54,5 +82,40 @@ class CapstoneRepository @Inject constructor(
         else Resource.Failure(message = result.message)
     }
 
+    suspend fun changeUserNickname(newNickname: String): Resource<String>{
+        val token = getSavedToken() ?: return Resource.Failure("token does not exist")
+        val result = try {
+            api.changeUserNickname(token.toTokenMap(), newNickname)
+        }catch (e: Exception) {
+            return Resource.Failure(message = e.message.toString())
+        }
+        return if (result.success)
+            Resource.Success(data = result.data)
+        else Resource.Failure(message = result.message)
+    }
+
+    suspend fun followUser(userId: Int): Resource<FollowData> {
+        val token = getSavedToken() ?: return Resource.Failure("token does not exist")
+        val result = try {
+            api.followUser(token.toTokenMap(), userId)
+        }catch (e: Exception) {
+            return Resource.Failure(message = e.message.toString())
+        }
+        return if (result.success)
+            Resource.Success(data = result.data)
+        else Resource.Failure(message = result.message)
+    }
+
+    suspend fun unfollowUser(userId: Int): Resource<String> {
+        val token = getSavedToken() ?: return Resource.Failure("token does not exist")
+        val result = try {
+            api.unfollowUser(token.toTokenMap(), userId)
+        }catch (e: Exception) {
+            return Resource.Failure(message = e.message.toString())
+        }
+        return if (result.success)
+            Resource.Success(data = result.data)
+        else Resource.Failure(message = result.message)
+    }
 
 }
