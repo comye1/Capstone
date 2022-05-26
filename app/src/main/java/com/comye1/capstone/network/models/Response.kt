0a7f6f7477baefdf4568d157f6b11ac00e0f7862
comye1 <comye1@seoultech.ac.kr>
2022-05-26 20:57:08 +0900

    응답 수정

--- app/src/main/java/com/comye1/capstone/network/models/Response.kt
+++ app/src/main/java/com/comye1/capstone/network/models/Response.kt
@@ -1,45 +1,138 @@
 package com.comye1.capstone.network.models
 
-
-sealed class Response<T>(
+data class BooleanResponse(
     val success: Boolean,
-    val data: T,
-    val message: String,
-    val error: String
+    val message: String = "",
+    val error: String = "",
+    val data: Boolean
+)
+
+data class SignInResponse(
+    val success: Boolean,
+    val message: String = "",
+    val error: String = "",
+    val token: String = "",
+    val data: SignUpData
+)
+
+
+data class StringResponse(
+    val success: Boolean,
+    val message: String = "",
+    val error: String = "",
+    val data: String
+)
+
+
+data class SignUpResponse(
+    val success: Boolean,
+    val message: String = "",
+    val error: String = "",
+    val data: SignUpData
 )
 
 data class SignUpData(
-    val id: Int,
-    val user_id: String,
-    val password: String,
-    val nickname: String,
+    val id: Int = -111111,
+    val user_id: String = "",
+    val password: String = "",
+    val nickname: String = "",
+)
+
+data class FollowResponse(
+    val success: Boolean,
+    val message: String = "",
+    val error: String = "",
+    val data: FollowData
+)
+
+data class FollowListResponse(
+    val success: Boolean,
+    val message: String = "",
+    val error: String = "",
+    val data: List<FollowData> = listOf()
 )
 
 data class FollowData(
-    val id: Int,
-    val follower: Int,
-    val following: Int,
-    val updatedAt: String,
-    val createdAt: String,
-    val User: SignUpData,
+    val id: Int = -111111,
+    val follower: Int = -111111,
+    val following: Int = -111111,
+    val updatedAt: String = "",
+    val createdAt: String = "",
+    val User: SignUpData = SignUpData(),
 )
 
+data class GoalResponse(
+    val success: Boolean,
+    val message: String = "",
+    val error: String = "",
+    val data: GoalData
+)
+
+data class GoalListResponse(
+    val success: Boolean,
+    val message: String = "",
+    val error: String = "",
+    val data: List<GoalData> = listOf()
+)
+
+
 data class GoalData(
-    val copied_from: Int, // -1이면 원본
-    val id: Int,
-    val user_id: String,
-    val goal_title: String,
-    val board_category: String,
-    val Plan: PlanData
+    val copied_from: Int = -111111, // -1이면 원본
+    val id: Int = -111111,
+    val user_id: String = "",
+    val goal_title: String = "",
+    val board_category: String = "",
+    val Plan: PlanData = PlanData()
+)
+
+data class PlanResponse(
+    val success: Boolean,
+    val message: String = "",
+    val error: String = "",
+    val data: PlanData
+)
+
+data class PlanListResponse(
+    val success: Boolean,
+    val message: String = "",
+    val error: String = "",
+    val data: List<PlanData>
 )
 
 data class PlanData(
-    val id: Int,
-    val goal_id: Int,
-    val plan_title: String,
-    val is_checked: Boolean,
-    val content: String,
-    val createdAt: String,
-    val updatedAt: String
+    val id: Int = -1111111,
+    val goal_id: Int = -111111,
+    val plan_title: String = "",
+    val is_checked: Boolean = false,
+    val content: String = "",
+    val createdAt: String = "",
+    val updatedAt: String = "",
 )
 
+data class LikeResponse(
+    val success: Boolean,
+    val message: String = "",
+    val error: String = "",
+    val data: LikeData
+)
+
+data class LikeListResponse(
+    val success: Boolean,
+    val message: String = "",
+    val error: String = "",
+    val data: List<LikeData> = listOf()
+)
+
+data class LikeData(
+    val id: Int = -111111,
+    val plan_id: Int = -11111,
+    val createdAt: String = "",
+    val updatedAt: String = ""
+)
+
+data class PlanIntListRespone(
+    val success: Boolean,
+    val message: String = "",
+    val error: String = "",
+    val data: List<Int> = listOf()
+)
--- app/src/main/java/com/comye1/capstone/repository/CapstoneRepository.kt
+++ app/src/main/java/com/comye1/capstone/repository/CapstoneRepository.kt
@@ -14,7 +14,22 @@ import javax.inject.Inject
 class CapstoneRepository @Inject constructor(
     val api: CapstoneApi,
     val context: Context
-){
+) {
+    private val categorySharedPref: SharedPreferences = context.getSharedPreferences(
+        "category",
+        Context.MODE_PRIVATE
+    )
+
+    private fun saveCategory(category: String) {
+        categorySharedPref.edit {
+            putString("CATEGORY", category)
+            commit()
+        }
+    }
+
+    private fun getSavedCategory(): String? = categorySharedPref.getString("CATEGORY", null)
+
+
     private val tokenSharedPref: SharedPreferences = context.getSharedPreferences(
         "token",
         Context.MODE_PRIVATE
@@ -27,16 +42,20 @@ class CapstoneRepository @Inject constructor(
         }
     }
 
+
     private fun getSavedToken(): String? = tokenSharedPref.getString("TOKEN", null)
 
-    private fun String.toTokenMap(): Map<String, String> = mapOf(Pair("Authorization", "Bearer $this"))
+    private fun String.toTokenMap(): Map<String, String> =
+        mapOf(Pair("Authorization", "Bearer $this"))
+
+    lateinit var user: SignUpData
 
     suspend fun idCheck(id: String): Resource<Boolean> {
         Log.d("repo 1", "called")
         val result = try {
             Log.d("repo 1", "success")
-            api.idCheck(IdBody(user_id = id))
-        }catch (e:Exception) {
+            api.idCheck(id)
+        } catch (e: Exception) {
             Log.d("repo 1", "exception ${e.message.toString()}")
             return Resource.Failure(message = e.message.toString())
         }
@@ -46,35 +65,51 @@ class CapstoneRepository @Inject constructor(
         else Resource.Failure(message = result.message)
     }
 
-    suspend fun signUpUser(id:String, password: String, nickname: String): Resource<SignUpData> {
+    suspend fun signUpUser(id: String, password: String, nickname: String): Resource<SignUpData> {
         val result = try {
             api.signUpUser(SignUpBody(id, password, nickname))
-        }catch (e: Exception) {
+        } catch (e: Exception) {
             return Resource.Failure(message = e.message.toString())
         }
-        return if (result.success)
-            Resource.Success(data = result.data)
-        else Resource.Failure(message = result.message)
+        return if (result.success) {
+            return Resource.Success(data = result.data)
+
+        } else Resource.Failure(message = result.message)
     }
 
-    suspend fun signInUser(id: String, password: String): Resource<String> {
+    suspend fun signInUser(id: String, password: String): Resource<SignUpData> {
         val result = try {
             api.signInUser(signInBody = SignInBody(id, password))
         } catch (e: Exception) {
             return Resource.Failure(message = e.message.toString())
         }
-        return if (result.success){
-            saveToken(result.data)
+        return if (result.success) {
+            saveToken(result.token)
+            user = result.data
             Resource.Success(data = result.data)
-        }
-        else Resource.Failure(message = result.message)
+        } else Resource.Failure(message = result.message)
     }
 
-    suspend fun validUser(): Resource<String> {
+    suspend fun getUser(token: String): Boolean {
+        Log.d("repo" , "$token getUser")
+        val result = try {
+            api.getUser(token.toTokenMap())
+        } catch (e: Exception) {
+            return false
+        }
+        return if (result.success) {
+            user = result.data
+            true
+        }
+        else return false
+    }
+
+
+    suspend fun getUserById(id: Int): Resource<SignUpData> {
         val token = getSavedToken() ?: return Resource.Failure("token does not exist")
         val result = try {
-            api.validUser(token.toTokenMap())
-        }catch (e: Exception) {
+            api.getUserById(token.toTokenMap(), id)
+        } catch (e: Exception) {
             return Resource.Failure(message = e.message.toString())
         }
         return if (result.success)
@@ -82,11 +117,11 @@ class CapstoneRepository @Inject constructor(
         else Resource.Failure(message = result.message)
     }
 
-    suspend fun changeUserNickname(newNickname: String): Resource<String>{
+    suspend fun changeUserNickname(newNickname: String): Resource<String> {
         val token = getSavedToken() ?: return Resource.Failure("token does not exist")
         val result = try {
             api.changeUserNickname(token.toTokenMap(), newNickname)
-        }catch (e: Exception) {
+        } catch (e: Exception) {
             return Resource.Failure(message = e.message.toString())
         }
         return if (result.success)
@@ -98,7 +133,7 @@ class CapstoneRepository @Inject constructor(
         val token = getSavedToken() ?: return Resource.Failure("token does not exist")
         val result = try {
             api.followUser(token.toTokenMap(), userId)
-        }catch (e: Exception) {
+        } catch (e: Exception) {
             return Resource.Failure(message = e.message.toString())
         }
         return if (result.success)
@@ -110,12 +145,169 @@ class CapstoneRepository @Inject constructor(
         val token = getSavedToken() ?: return Resource.Failure("token does not exist")
         val result = try {
             api.unfollowUser(token.toTokenMap(), userId)
-        }catch (e: Exception) {
+        } catch (e: Exception) {
             return Resource.Failure(message = e.message.toString())
         }
         return if (result.success)
             Resource.Success(data = result.data)
         else Resource.Failure(message = result.message)
     }
+
+    suspend fun getFollowingList(userId: Int): Resource<List<FollowData>> {
+        val token = getSavedToken() ?: return Resource.Failure("token does not exist")
+        val result = try {
+            api.getFollowings(token.toTokenMap(), userId)
+        } catch (e: Exception) {
+            return Resource.Failure(message = e.message.toString())
+        }
+        return if (result.success)
+            Resource.Success(data = result.data)
+        else Resource.Failure(message = result.message)
+    }
+
+    suspend fun getFollowerList(userId: Int): Resource<List<FollowData>> {
+        val token = getSavedToken() ?: return Resource.Failure("token does not exist")
+        val result = try {
+            api.getFollowers(token.toTokenMap(), userId)
+        } catch (e: Exception) {
+            return Resource.Failure(message = e.message.toString())
+        }
+        return if (result.success)
+            Resource.Success(data = result.data)
+        else Resource.Failure(message = result.message)
+    }
+
+    suspend fun postGoal(goal: GoalBody): Resource<GoalData> {
+        val token = getSavedToken() ?: return Resource.Failure("token does not exist")
+        val result = try {
+            api.postGoal(token.toTokenMap(), goal)
+        } catch (e: Exception) {
+            return Resource.Failure(message = e.message.toString())
+        }
+        return if (result.success)
+            Resource.Success(data = result.data)
+        else Resource.Failure(message = result.message)
+    }
+
+    suspend fun getUserGoals(): Resource<List<GoalData>> {
+        val token = getSavedToken() ?: return Resource.Failure("token does not exist")
+        val result = try {
+            api.getUserGoals(token.toTokenMap())
+        } catch (e: Exception) {
+            return Resource.Failure(message = e.message.toString())
+        }
+        return if (result.success)
+            Resource.Success(data = result.data)
+        else Resource.Failure(message = result.message)
+    }
+
+    suspend fun editGoal(goal: EditGoalBody): Resource<String> {
+        val token = getSavedToken() ?: return Resource.Failure("token does not exist")
+        val result = try {
+            api.editGoal(token.toTokenMap(), goal)
+        } catch (e: Exception) {
+            return Resource.Failure(message = e.message.toString())
+        }
+        return if (result.success)
+            Resource.Success(data = result.data)
+        else Resource.Failure(message = result.message)
+    }
+
+    suspend fun getGoalById(id: Int): Resource<GoalData> {
+        val token = getSavedToken() ?: return Resource.Failure("token does not exist")
+        val result = try {
+            api.getGoalById(token.toTokenMap(), id)
+        } catch (e: Exception) {
+            return Resource.Failure(message = e.message.toString())
+        }
+        return if (result.success)
+            Resource.Success(data = result.data)
+        else Resource.Failure(message = result.message)
+    }
+
+    suspend fun postPlan(plan: PlanBody): Resource<PlanData> {
+        val token = getSavedToken() ?: return Resource.Failure("token does not exist")
+        val result = try {
+            api.postPlan(token.toTokenMap(), plan)
+        } catch (e: Exception) {
+            return Resource.Failure(message = e.message.toString())
+        }
+        return if (result.success)
+            Resource.Success(data = result.data)
+        else Resource.Failure(message = result.message)
+    }
+
+    suspend fun getUserPlans(): Resource<List<PlanData>> {
+        val token = getSavedToken() ?: return Resource.Failure("token does not exist")
+        val result = try {
+            api.getUserPlans(token.toTokenMap())
+        } catch (e: Exception) {
+            return Resource.Failure(message = e.message.toString())
+        }
+        return if (result.success)
+            Resource.Success(data = result.data)
+        else Resource.Failure(message = result.message)
+    }
+
+    suspend fun editPlan(plan: EditPlanBody): Resource<List<Int>> {
+        val token = getSavedToken() ?: return Resource.Failure("token does not exist")
+        val result = try {
+            api.editPlan(token.toTokenMap(), plan)
+        } catch (e: Exception) {
+            return Resource.Failure(message = e.message.toString())
+        }
+        return if (result.success)
+            Resource.Success(data = result.data)
+        else Resource.Failure(message = result.message)
+    }
+
+    suspend fun getPlanById(goalId: Int, planId: Int): Resource<GoalPlanResponse> {
+        val token = getSavedToken() ?: return Resource.Failure("token does not exist")
+        val result = try {
+            api.getPlanById(token.toTokenMap(), goalId, planId)
+        } catch (e: Exception) {
+            return Resource.Failure(message = e.message.toString())
+        }
+        return if (result.success)
+            Resource.Success(data = result)
+        else Resource.Failure(message = result.message)
+    }
+
+    suspend fun completePlan(goalId: Int, planId: Int): Resource<List<Int>> {
+        val token = getSavedToken() ?: return Resource.Failure("token does not exist")
+        val result = try {
+            api.completePlan(token.toTokenMap(), goal_id = goalId, plan_id = planId)
+        } catch (e: Exception) {
+            return Resource.Failure(message = e.message.toString())
+        }
+        return if (result.success)
+            Resource.Success(data = result.data)
+        else Resource.Failure(message = result.message)
+    }
+
+    suspend fun likePlan(goalId: Int, planId: Int): Resource<LikeData> {
+        val token = getSavedToken() ?: return Resource.Failure("token does not exist")
+        val result = try {
+            api.likePlan(token.toTokenMap(), goal_id = goalId, plan_id = planId)
+        } catch (e: Exception) {
+            return Resource.Failure(message = e.message.toString())
+        }
+        return if (result.success)
+            Resource.Success(data = result.data)
+        else Resource.Failure(message = result.message)
+    }
+
+    suspend fun getLikes(goalId: Int, planId: Int): Resource<List<LikeData>> {
+        val token = getSavedToken() ?: return Resource.Failure("token does not exist")
+        val result = try {
+            api.getLikes(token.toTokenMap(), goal_id = goalId, plan_id = planId)
+        } catch (e: Exception) {
+            return Resource.Failure(message = e.message.toString())
+        }
+        return if (result.success)
+            Resource.Success(data = result.data)
+        else Resource.Failure(message = result.message)
+    }
+
 
 }
